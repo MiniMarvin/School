035caa9ac249bf6acc2f5ed4b773f72c421a2dcf
Caio Moreira Gomes <caiogomes@MiniMarvin>
2015-12-29 19:01:23 -0300

    adição de comentários para doxygen

--- noFunGame/modules/graphiclib.c
+++ noFunGame/modules/graphiclib.c
@@ -31,12 +31,12 @@ void flip(ALLEGRO_BITMAP* dbuf, ALLEGRO_DISPLAY* display){
 }
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief puts a red dot in the determined point of a bitmap
+ * @details places a red dot in the position specified by (x,y) in the bitmap
  * 
- * @param x [description]
- * @param y [description]
- * @param dbuf [description]
+ * @param x x axys
+ * @param y y axys
+ * @param dbuf bitmap where the dot will be placed
  */
 void red_dot(int x, int y, ALLEGRO_BITMAP* dbuf){
 
@@ -45,7 +45,15 @@ void red_dot(int x, int y, ALLEGRO_BITMAP* dbuf){
 
 }
 
-//functoin that draws a cartesian graph structure
+/**
+ * @brief create a cartesian graphic structure in a bitmap
+ * @details draw two lines to make the axys from the cartesian graphic in the passed bitmap
+ * 
+ * @param bitmap the bitmap where the cartesian axys will be added
+ * @param cor the color of the axys
+ * 
+ * @return an array saying the zeros from the graphic (x0,y0) in pixels of this bitmap
+ */
 int* drawcartesian(ALLEGRO_BITMAP *bitmap, ALLEGRO_COLOR cor){
 	al_set_target_bitmap(bitmap);
 	
@@ -63,7 +71,15 @@ int* drawcartesian(ALLEGRO_BITMAP *bitmap, ALLEGRO_COLOR cor){
 
 }
 
-//function to write a function denoted by f(point) in cartesian graphic
+/**
+ * @brief draws a graphic over an cartesian structure
+ * @details draws in a bitmap a graphic from one f(x) represented for an array
+ * 
+ * @param points the array that have the points that will be placed in the graphic
+ * @param bitmap the bitmap where the graphic will be drawn
+ * @param arraySize the quantity of points that have to be drawn
+ * @param cor the color of the graphic
+ */
 void drawGraphic(double* points, ALLEGRO_BITMAP *bitmap, int arraySize, ALLEGRO_COLOR cor){
 
 	int i = 0;
@@ -97,14 +113,23 @@ void drawGraphic(double* points, ALLEGRO_BITMAP *bitmap, int arraySize, ALLEGRO_
 }
 
 //get the maximum value in an array
+/**
+ * @brief get the max value inside an array
+ * @details get the biggest value inside an one dimension array
+ * 
+ * @param points the pointer to the array
+ * @param size the size of the array(avoid any kind of segmentation fault)
+ * 
+ * @return the max value
+ */
 int getMax(double* points, int size){
 
 	int max = 0;
-   int i = 0;
+	int i = 0;
 
-   for(i = 0; i < size; i++){
-      if(max < points[i]) max = points[i];
-   }
+	for(i = 0; i < size; i++){
+		if(max < points[i]) max = points[i];
+	}
 
 	return max;
 }
--- noFunGame/modules/graphiclib.h
+++ noFunGame/modules/graphiclib.h
@@ -9,25 +9,65 @@
 
 	#include "common.c"
 
-	//draws an ordenated axys to the cartesian map
+	/**
+	 * @brief Start allegro
+	 * @details Start allegro lib addons that will be necessary in the script
+	 */
+	void allegroStart();
+
+	/**
+	 * @brief plot a bitmap in a windows
+	 * @details plot a received bitmap in a displaying windows, just can be used in GUI modes
+	 * 
+	 * @param dbuf the bitmap which will be ploted
+	 * @param display the window where the bitmap will be displayed
+	 */
+	void flip(ALLEGRO_BITMAP* dbuf, ALLEGRO_DISPLAY* display);
+
+	/**
+	 * @brief puts a red dot in the determined point of a bitmap
+	 * @details places a red dot in the position specified by (x,y) in the bitmap
+	 * 
+	 * @param x x axys
+	 * @param y y axys
+	 * @param dbuf bitmap where the dot will be placed
+	 */
+	void red_dot(int x, int y, ALLEGRO_BITMAP* dbuf);
+
+	/**
+	 * @brief create a cartesian graphic structure in a bitmap
+	 * @details draw two lines to make the axys from the cartesian graphic in the passed bitmap
+	 * 
+	 * @param bitmap the bitmap where the cartesian axys will be added
+	 * @param cor the color of the axys
+	 * 
+	 * @return an array saying the zeros from the graphic (x0,y0) in pixels of this bitmap
+	 */
 	int* drawcartesian(ALLEGRO_BITMAP *bitmap, ALLEGRO_COLOR cor);
 	
-	//draws a Graphic over the cartesian
+	/**
+	 * @brief draws a graphic over an cartesian structure
+	 * @details draws in a bitmap a graphic from one f(x) represented for an array
+	 * 
+	 * @param points the array that have the points that will be placed in the graphic
+	 * @param bitmap the bitmap where the graphic will be drawn
+	 * @param arraySize the quantity of points that have to be drawn
+	 * @param cor the color of the graphic
+	 */
 	void drawGraphic(double* points, ALLEGRO_BITMAP *bitmap, int arraySize, ALLEGRO_COLOR cor);
 	
+	/**
+	 * @brief get the max value inside an array
+	 * @details get the biggest value inside an one dimension array
+	 * 
+	 * @param points the pointer to the array
+	 * @param size the size of the array(avoid any kind of segmentation fault)
+	 * 
+	 * @return the max value
+	 */
+	int getMax(double* points, int size);
+
 	//get the size from an array from a pointer
 	int getArraySize(double* array);
 	
-	//get the max value in an array
-	int getMax(double* points, int size);
-	
-	//draws a red dot in a point on a bitmap
-	void red_dot(int x, int y, ALLEGRO_BITMAP* dbuf);
-
-	//init the allegro add-ons necessary to the program
-	void allegroStart();
-
-	//plot the bitmap passed in a window
-	void flip(ALLEGRO_BITMAP* dbuf, ALLEGRO_DISPLAY* display);
-
 #endif

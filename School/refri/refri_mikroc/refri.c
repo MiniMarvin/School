8cf01deb04f50d8bf54310ba6d51cca91897b0af
minimarvin <caiogomes34@gmail.com>
2016-06-28 17:16:39 +0000

    Adição de funções para caso não haja dinheiro ou latas o suficiente.

--- School/refri/refri_mikroc/refri.c
+++ School/refri/refri_mikroc/refri.c
@@ -1,24 +1,26 @@
-sbit add_25		at	P1_0_bit;
-sbit add_50		at	P1_1_bit;
-sbit add_100	at	P1_2_bit;
-sbit select 	at	P3_6_bit;
-sbit abortit	at	P3_7_bit;
-sbit next		at 	P1_4_bit;
-sbit back		at 	P1_4_bit;
+sbit add_25                at        P1_0_bit;
+sbit add_50                at        P1_1_bit;
+sbit add_100        at        P1_2_bit;
+sbit select         at        P3_6_bit;
+sbit abortit        at        P3_7_bit;
+sbit next                at         P3_5_bit;
+sbit back                at         P3_2_bit;
 
-sbit tipo1 		at 	P3_1_bit;
+sbit tipo1                 at         P3_1_bit;
 
-#include "background.c"
+//#include "background.c"
+#include "images.c"
 
 ///Function prototypes
 unsigned short check(unsigned short pt);
 void configit();
 void selectRefri();
 void returnMoney();
+void noMoney();
 
 
 //global variables
-unsigned float total = 0.0;
+int total = 0;
 unsigned short selectFlag = 0;
 unsigned short i;
 
@@ -38,16 +40,28 @@ const code unsigned char Const_Text1[] = "Preco Refri: R$3.50";
 const code unsigned char Const_Text2[] = "Selecione o Refri";
 const code unsigned char Const_Text3[] = "Atual: ";
 
-const code unsigned char Const_Text4[] = "Trabalho apresentado por:";
+
+const code unsigned char Const_Text4[] = "Apresentado por:";
 const code unsigned char Const_Text5[] = "Caio Gomes";
 const code unsigned char Const_Text6[] = "Michael Barney";
 const code unsigned char Const_Text7[] = "Otacilio Neto";
 
+const code unsigned char Const_Text8[] = "Operacao Cancelada.";
+const code unsigned char Const_Text9[] = "Valor retornado:   ";
+const code unsigned char Const_Text10[] = "P. ENTER TO CONTINUE";
+const code unsigned char Const_Text11[] = "Qtd. insuficiente";
+const code unsigned char Const_Text12[] = "Saldo insuficiente";
+
 const code unsigned char nomeRefri_0[] = "Coca: ";
-const code unsigned char nomeRefri_1[] = "Pespsi";
-const code unsigned char nomeRefri_2[] = "Fanta";
-const code unsigned char nomeRefri_3[] = "Sprite";
+const code unsigned char nomeRefri_1[] = "Pespsi:";
+const code unsigned char nomeRefri_2[] = "Fanta:";
+const code unsigned char nomeRefri_3[] = "Sprite:";
 char* vartxt;
+
+//images
+//const unsigned char caio_bmp[1024];
+//const unsigned char michael_bmp[1024];
+//const unsigned char otacilio_bmp[1024];
 
 //extern unsigned char const background_bmp[1024];
 //extern unsigned char const caio_bmp[1024];
@@ -58,68 +72,79 @@ char* vartxt;
 char* codetxt_to_ramtxt(const char* ctxt);
 void atualizaP();
 void creditos();
-unsigned short refris[4] = {20, 20, 20, 20};
+unsigned short refris[4] = {5, 5, 5, 5};//{20, 20, 20, 20};
 unsigned short rselect = 0;
 unsigned int buff = 0;
+void nonRefri();
+void paintRefri();
+void writeGLCDTextCenter(const char* text, unsigned short linha, unsigned short cor);
 
 void main() {
-	Glcd_Init();                              // Initialize GLCD
-	configit();
-	//P2 = 0xFF;
+        Glcd_Init();                              // Initialize GLCD
+        configit();
+        //atualizaP();
+        //P2 = 0xFF;
 
-	selectFlag = 1;
+        selectFlag = 1;
 
-	while(1){
+        while(1){
 
-		//check the refrigerant selection buttons
-		if( check(next) ) {
-			if( rselect++ >= 4) rselect = 0;
-			atualizaP();
-			while( check(next) );
-		}
+                //check the refrigerant selection buttons
+                if( check(next) ) {
+                        if( ++rselect > 3) rselect = 0;
+                        //atualizaP();
+                        configit();
+                        while( check(next) );
+                }
 
-		if( check(back) ) {
-			if( rselect-- >= 4) rselect = 3;
-			atualizaP();
-			while( check(next) );
-		}
+                if( check(back) ) {
+                        if( --rselect > 10) rselect = 3;
+                        //atualizaP();
+                        configit();
+                        while( check(back) );
+                }
 
-		//check the buttons from adding.
-		if( check(add_25) ) {
-			total = total + 0.25;
-			buff += 25;
-			atualizaP();
-			while( check(add_25) );
-		}
+                //check the buttons from adding.
+                if( check(add_25) ) {
+                        total = total + 25;
+                        buff = buff + 25;
+                        atualizaP();
+                        while( check(add_25) );
+                }
 
-		if( check(add_50) ) {
-			total =  total + 0.50;
-			buff += 50;
-			atualizaP();
-			while( check(add_50) );
-		}
-		if( check(add_100) ) {
-			total = total + 1.00;
-			buff += 100;
-			atualizaP();
-			while( check(add_100) );
-		}
+                if( check(add_50) ) {
+                        total =  total + 50;
+                        buff = buff + 50;
+                        atualizaP();
+                        while( check(add_50) );
+                }
+                if( check(add_100) ) {
+                        total = total + 100;
+                        buff = buff + 100;
+                        atualizaP();
+                        while( check(add_100) );
+                }
 
-		//check if it's going to abort the operation.
-		if( check(abortit) ) {
-			returnMoney()
-		}
+                //check if it's going to abort the operation.
+                if( check(abortit) ) {
+                        returnMoney();
+                }
 
-		//check if it has already enought money to choose a refrigerant.
-		if(total >= 3.50 && check(select)){
-			if(total >= 10.0){
-				creditos();
-				while( !check(abortit) );
-			}
-			selectRefri();
-		}
+                //check if it has already enought money to choose a refrigerant.
+                if(total >= 350 && check(select)) {
+                        if(total >= 1000){
+                                Glcd_Fill(0x00); 
+                                //Glcd_Write_Text("creditos", 1,0,1);
+                                creditos();
+                                while( !check(abortit) );
+                        }
+                        selectRefri();
+                }
+                else if(total <= 350 && check(select)) {
+                        noMoney();
+                }
 
-	}
+        }
 }
 
 /**
@@ -132,11 +157,11 @@ void main() {
 
 unsigned short check(unsigned short pt) {
          //P1 = 0xFF;
-	if(!pt) {
-		Delay_ms(1);
-		if(!pt) return 1;
-	}
-	return 0;
+        if(!pt) {
+                Delay_ms(1);
+                if(!pt) return 1;
+        }
+        return 0;
 }
 
  
@@ -145,43 +170,90 @@ unsigned short check(unsigned short pt) {
   * @details [long description]
   */
 void atualizaP() {
-	
-	unsigned short i = 0;
-	
-	FloatToStr(total, vartxt);
-	Glcd_Write_Text(vartxt, 56,1,1);
-	
-	for(i = 0; i< 4; i++){
-		FloatToStr( refris[i], vartxt );
-		Glcd_Write_Text(vartxt, 56,i+2,1);
-	}
-	
-	
-	
+        /*
+        IntToStr( total, vartxt);
+        Glcd_Write_Text(vartxt, 56,1,1);
+        
+        ShortToStr( refris[0], vartxt );
+        Glcd_Write_Text(vartxt, 56,2,1);
+        ShortToStr( refris[1], vartxt );
+        Glcd_Write_Text(vartxt, 56,3,1);
+        ShortToStr( refris[2], vartxt );
+        Glcd_Write_Text(vartxt, 56,4,1);
+        ShortToStr( refris[3], vartxt );
+        Glcd_Write_Text(vartxt, 56,5,1);
+
+        Glcd_Write_Text("  ", 88,2,1);
+        Glcd_Write_Text("  ", 88,3,1);
+        Glcd_Write_Text("  ", 88,4,1);
+        Glcd_Write_Text("  ", 88,5,1);
+        Glcd_Write_Text("*", 90,rselect+2,1);
+        */
+        
+        Glcd_Write_Text(" ", 3,1,1);
+
+        IntToStr( total, vartxt-2);
+        Glcd_Write_Text(vartxt, 3,1,1);
+        
+        Glcd_Write_Text(" ", 110,4,1);
+        
+        ShortToStr( refris[rselect], vartxt-1);
+        Glcd_Write_Text(vartxt, 100,4,1);
+        
+        paintRefri();
+
+        //Glcd_Write_Text(" ", 1,2,1);
+        //Glcd_Write_Text(" ", 1,3,1);
+        //Glcd_Write_Text(" ", 1,4,1);
+        //Glcd_Write_Text(" ", 1,5,1);
+        //Glcd_Write_Text("*", 1,rselect+2,1);
+        
+        
 }     
+
+void paintRefri() {
+    //unsigned short i = 0;
+    //unsigned short j = 0;
+    //unsigned short px = 0;
+    //Glcd_Rectangle(unsigned short x_upper_left, unsigned short y_upper_left, unsigned short x_bottom_right, unsigned short y_bottom_right, unsigned short color);
+    //Glcd_Rectangle(19+11*rselect, 1, 30+11*rselect, 29, 1);
+    Glcd_Rectangle(1,19+11*rselect, 28,30+11*rselect - 1, 1);
+
+    /*
+    for (i = 19 + 11*rselect; i < 30 + 11*rselect; ++i)
+    {
+        for (j = 1; j < 29; ++j)
+        {
+        	px = !background_bmp[128*j+64*i];
+            Glcd_Dot(j, i, px);//1);
+        }
+    }
+    */
+}
 
 /**
  * @brief Initial config of the system.
  * @details Configure the initial bits registers and operations mode in the system.
  */
 void configit() {
-	
-	unsigned short i = 0;
-	
-	//Glcd_Init();                              // Initialize GLCD
-	Glcd_Fill(0x00);                          // Clear GLCD
-	Glcd_Set_Font(font5x7, 5, 7, 32);
-	
-  	Glcd_Write_Text(codetxt_to_ramtxt(Const_Text1), 1,0,1);
-  	Glcd_Write_Text(codetxt_to_ramtxt(Const_Text3), 1,1,1);
-  	
-	Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_0 ), 1,2,1);
-	Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_1 ), 1,3,1);
-	Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_2 ), 1,4,1);
-	Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_3 ), 1,5,1);
-  	
-  	
-  	atualizaP();
+        
+        unsigned short i = 0;
+        
+        //Glcd_Init();                              // Initialize GLCD
+        Glcd_Fill(0x00);                          // Clear GLCD
+        Glcd_Set_Font(font5x7, 5, 7, 32);
+        
+          //Glcd_Write_Text(codetxt_to_ramtxt(Const_Text1), 1,0,1);
+          //Glcd_Write_Text(codetxt_to_ramtxt(Const_Text3), 1,1,1);
+          
+        //Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_0 ), 1,2,1);
+        //Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_1 ), 1,3,1);
+        //Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_2 ), 1,4,1);
+        //Glcd_Write_Text(codetxt_to_ramtxt( nomeRefri_3 ), 1,5,1);
+        
+        Glcd_Image(background_bmp);
+          
+        atualizaP();
 
 }
 
@@ -190,13 +262,40 @@ void configit() {
  * @details The selection section in GLCD and in the system, waiting for the refri selection.
  */
 void selectRefri() {
-	total -= 3.5;
-	if(refris[rselect] != 0) refris[rselect]--;
-	else nonRefri();
+        total -= 350;
+        buff -= 350;
+        if(refris[rselect] != 0) {
+                refris[rselect]--;
+                configit();
+                
+                Glcd_Image(background1_bmp);
+                atualizaP();
+                delay_ms(500);
+                
+                Glcd_Image(background2_bmp);
+                atualizaP();
+                delay_ms(500);
+                
+                Glcd_Image(background3_bmp);
+                atualizaP();
+                delay_ms(500);
+                
+                while( !check(select) );
+                
+                configit();
+        }
+        else nonRefri();
 }
 
+/**
+ * 
+ */
 void nonRefri(){
-
+        writeGLCDTextCenter(Const_Text11, 5, 1);
+        writeGLCDTextCenter(Const_Text10, 6, 1);
+        while(check(select));
+        while(!check(select));
+        configit();
 }
 
 /**
@@ -204,7 +303,19 @@ void nonRefri(){
  * @details Show in the graphic interface that it's going to return the money.
  */
 void returnMoney() {
-	total -= buff/100.0;
+
+        Glcd_Fill(0x00);
+        writeGLCDTextCenter(Const_Text8, 2, 1);
+        writeGLCDTextCenter(Const_Text9, 3, 1);
+        writeGLCDTextCenter(Const_Text10, 5, 1);
+        IntToStr(buff, vartxt);
+        Glcd_Write_Text(vartxt, 80, 3, 1);
+        while(!check(select));
+
+        if(total >= buff) total = total - buff;
+        buff = 0;
+
+        configit();
 }
 
 
@@ -233,121 +344,69 @@ char* codetxt_to_ramtxt(const char* ctxt){
  * @param charNum the number of characters in the text.
  * @param cor The color of the text.
  */
-void writeGLCDTextCenter(unsigned char* txt, unsigned short linha, unsigned short cor) {
-	unsigned short charNum = 0;
-	while(txt[charNum++]);
-	Glcd_Write_Text(txt, 58 - charNum*5/2, linha, cor);
+void writeGLCDTextCenter(const char* text, unsigned short linha, unsigned short cor) {
+        unsigned char* txt;
+        unsigned short charNum = 0;
+
+        txt = codetxt_to_ramtxt(text);
+        
+        while(txt[charNum++]);
+        Glcd_Write_Text(txt, 58 - charNum*5/2, linha, cor);
 }
 
 //......other declarations and functions
 
-
-/**
- * @brief Convert a float value to a string
- * @details Convert a float value to a string with a determined precision after the 
- * 
- * @param myData [description]
- * @param char [description]
- * 
- * @return [description]
- */
-unsigned char* floatToChar(float myData, unsigned char precisionRight){
-
-    float aux = myData;
-	unsigned int auxInt = 0;
-	unsigned char b = (int) myData;
-	unsigned char precisionLeft = 1;
-	static unsigned char charater[18];
-    unsigned char c = 0;
-
-
-	//determina quantos caracteres existem antes da vírgula
-	while(b /= 10){
-		precisionLeft++;
-	}
-
-	b = 0;
-
-	//coloca no array os números antes da vírgula
-	while(b < precisionLeft){
-		c = precisionLeft - 1 - b;
-		aux = myData;
-		//retrocede o número de casas decimais necessário
-		//ex: 10.75(antes do loop) 1.075 após o loop
-		while(c){
-			aux /= 10;
-            c--;
-		}
-		//retira o último dígito do número inteiro
-		//ex: 107.5 (antes do loop) 7 após o loop
-		auxInt = (int) aux;
-		auxInt %= 10;
-		charater[b] = auxInt + '0';
-		b++;
-	}
-
-
-	//caso o usuário deseje ver valores após a vírgula
-	if(precisionRight){
-		//coloca uma vírgula no array
-		charater[b] = '.';
-
-		//coloca no array os valores após a vírgula
-		while(++b <= precisionLeft + precisionRight){
-			c = b - precisionLeft;
-            auxInt = 0;//(int) myData;
-			aux = myData - auxInt;
-			//avança o número de casas decimais necessário
-			//ex: 10.75(antes do loop) 107.5 após o loop
-			while(c--){
-				aux *= 10;
-			}
-			//retira o último dígito do número inteiro
-			//ex: 107.5 (antes do loop) 7 após o loop
-			auxInt = (int) aux;
-			auxInt %= 10;
-			charater[b] = auxInt + '0';
-		}
-	}
-	charater[b] = 0;
-	return charater;
-}
-
-
 //......other declarations and functions
-/*
+
 void creditos()
 {
-	Glcd_Fill(0x00); 
-	Glcd_Set_Font(Character8x7, 8, 7, 32);
-	Glcd_Write_Text("Trabalho apresentado por:", 1, 3, 2);
-	Delay_ms(2000);
-	
-	Glcd_Fill(0x00); 
-	Glcd_Set_Font(Character8x7, 8, 7, 32);
-	Glcd_Write_Text("Caio Gomes", 1, 3, 2);
-	Delay_ms(2000);
-	
-	Glcd_Fill(0x00); 
-	Glcd_Image(caio_bmp);
-	Delay_ms(2000);
-	
-	Glcd_Fill(0x00); 
-	Glcd_Set_Font(Character8x7, 8, 7, 32);
-	Glcd_Write_Text("Michael Barney", 1, 3, 2);
-	Delay_ms(2000);
-	
-	Glcd_Fill(0x00); 
-	Glcd_Image(michael_bmp);
-	Delay_ms(2000);
+        Glcd_Fill(0x00); 
+        Glcd_Set_Font(Character8x7, 8, 7, 32);
+        //Glcd_Write_Text( codetxt_to_ramtxt(Const_Text4), 1, 3, 2);
+        writeGLCDTextCenter(Const_Text4, 3, 2);
+        Delay_ms(2000);
+        
+        Glcd_Fill(0x00); 
+        Glcd_Set_Font(Character8x7, 8, 7, 32);
+        Glcd_Write_Text( codetxt_to_ramtxt(Const_Text5), 1, 3, 2);
+        writeGLCDTextCenter(Const_Text5, 3, 2);
+        Delay_ms(2000);
+        
+        /*
+        Glcd_Fill(0x00); 
+        Glcd_Image(background_bmp);
+        Delay_ms(2000);
+        */
+        
+        //Glcd_Fill(0x00); 
+        //Glcd_Image(caio_bmp);
+        //Delay_ms(2000);
+        
+        Glcd_Fill(0x00); 
+        Glcd_Set_Font(Character8x7, 8, 7, 32);
+        //Glcd_Write_Text(codetxt_to_ramtxt(Const_Text6), 1, 3, 2);
+        writeGLCDTextCenter(Const_Text6, 3, 2);
+        Delay_ms(2000);
+        
+        Glcd_Fill(0x00); 
+        Glcd_Image(michael_bmp);
+        Delay_ms(2000);
 
-	Glcd_Fill(0x00); 
-	Glcd_Set_Font(Character8x7, 8, 7, 32);
-	Glcd_Write_Text("Otacilio Neto", 1, 3, 2);
-	Delay_ms(2000);
-	
-	Glcd_Fill(0x00); 
-	Glcd_Image(otacilio_bmp);
-	Delay_ms(2000);
+        Glcd_Fill(0x00); 
+        Glcd_Set_Font(Character8x7, 8, 7, 32);
+        //Glcd_Write_Text(codetxt_to_ramtxt(Const_Text7), 1, 3, 2);
+        writeGLCDTextCenter(Const_Text7, 3, 2);
+        Delay_ms(2000);
+        
+        Glcd_Fill(0x00); 
+        Glcd_Image(otacilio_bmp);
+        Delay_ms(2000);
 }
-/*
+
+void noMoney() {
+        writeGLCDTextCenter(Const_Text12, 5, 1);
+        writeGLCDTextCenter(Const_Text10, 6, 1);
+        while(check(select));
+        while(!check(select));
+        configit();
+}
